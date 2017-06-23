a7a17641e454b08fb3a88f6b6857ecc3d80ad8de
rbyirdaw <yrobelb@gmail.com>
2017-06-23 17:12:38 -0500

    continuing with controller specs

--- test/spec/usDriversControllerSpec.js
+++ test/spec/usDriversControllerSpec.js
@@ -20,33 +20,52 @@ describe ("USdriversController", function() {
   });
 
 
-  describe("when gender is selected", function() {
+  xdescribe("when gender is selected", function() {
     it("should set corresponding flag", function() {
       controller.update({showMaleDrivers: true});
       expect(controller.maleDrivers.show).toEqual(true);
     });
   });
 
-  describe("when gender is de-selected", function() {
+  xdescribe("when gender is de-selected", function() {
     it("should un-set corresponding flag", function() {
       controller.update({showMaleDrivers: false});
       expect(controller.maleDrivers.show).toEqual(false);
     });
   });
 
+  xdescribe("when age group is selected,", function() {
+    beforeEach(function() {
+      spyOn(controller, 'getAgeGroupCounts');
+      spyOn(controller, 'updateDriverTotals');
+    });
+    it("should add age group's count to total", function() {
+      controller.update({ageGroup: "20_24", show: true});
+      expect(controller.getAgeGroupCounts).toHaveBeenCalled();
+      expect(controller.updateDriverTotals).toHaveBeenCalled();
+    });
+  });
+
   describe("when age group is selected,", function() {
 
-    describe("and male driver count is shown,", function() {
-      beforeEach(function() {
-        spyOn(controller, 'getAgeGroupCounts');
-        spyOn(controller, 'updateDriverTotals');
+    //need some data first
+    beforeEach(function(done) {
+      controller.model.read(1999, function(d) {
+        controller.model.setData(d);
+        done();
       });
-      it("should add age group's count to total", function() {
-        controller.update({ageGroup: "20_24", show: true});
-        expect(controller.getAgeGroupCounts).toHaveBeenCalled();
-        expect(controller.updateDriverTotals).toHaveBeenCalled();
-      })
-    })
+    });
+    describe("and male driver count is shown,", function() {
+      var ageGroupObj = {ageGroup: "20_24", show: true};
+
+      it("should retrieve counts for selected age group", function() {
+        var currData = controller.model.getData(),
+            counts = controller.getAgeGroupCounts(
+                  ageGroupObj.ageGroup, currData.maleDrivers);
+        expect(counts.length).toEqual(51);
+      });
+
+    });
 
   });
 
--- usDriversController.js
+++ usDriversController.js
@@ -13,6 +13,7 @@
     }
   }
 
+  //============================================================================
   USdriversController.prototype.update = function(updateObj) {
     var self = this;
 
@@ -22,21 +23,75 @@
       });
 
     } else if (updateObj.hasOwnProperty('ageGroup')) {
-      for (ageGroup in updateObj.ageGroups) {
-        console.log("ageGroup is "+ageGroup);
+      console.log("ageGroup is "+updateObj.ageGroup);
+
+      var currentData = this.model.getData(),
+          maleDriverCounts,
+          femaleDriverCounts;
+
+      if (this.maleDrivers.show) {
+        maleDriverCounts =
+            this.getAgeGroupCounts(updateObj.ageGroup, currentData.maleDrivers);
       }
-      var currentData = this.model.getData();
-      this.getAgeGroupCounts();
+
+      if (this.femaleDrivers.show) {
+        femaleDriverCounts =
+            this.getAgeGroupCounts(updateObj.ageGroup, currentData.femaleDrivers);
+      }
+
+      this.updateDriverTotals();
 
     } else if (updateObj.hasOwnProperty('showMaleDrivers')) {
       this.maleDrivers.show = updateObj.showMaleDrivers;
     }
   };
 
-  USdriversController.prototype.getAgeGroupCounts = function() {
+  //============================================================================
+  USdriversController.prototype.getAgeGroupCounts =
+      function(ageGroup, dataByGender) {
+        if (dataByGender.length !== 51) {
+          console.log("Incorrect data type [getAgeGroupCounts]");
+          return;
+        } else {
+          var ageGroupIndex = this.ageGroupMap.get(ageGroup),
+              ageGroupCounts = [];
 
+          if (ageGroupIndex === undefined) {
+            console.log("Age ground index not found [getAgeGroupCounts]");
+            return;
+
+          } else {
+            dataByGender.forEach(function(e) {
+              ageGroupCounts.push(e.age_group_count[ageGroupIndex].count);
+            });
+
+            return ageGroupCounts;
+          }
+        }
   };
 
+  //============================================================================
+  USdriversController.prototype.updateDriverTotals = function() {
+
+  };
+  //============================================================================
+  USdriversController.prototype.ageGroupMap = new Map([
+    ["19_and_under", 0],
+    ["20_24", 1],
+    ["25_29", 2],
+    ["30_34", 3],
+    ["35_39", 4],
+    ["40_44", 5],
+    ["45_49", 6],
+    ["50_54", 7],
+    ["55_59", 8],
+    ["60_64", 9],
+    ["65_69", 10],
+    ["70_74", 11],
+    ["75_79", 12],
+    ["80_84", 13],
+    ["85_and_over", 14]
+  ])
 
 
   window.usDriversApp = usDriversApp || {};
